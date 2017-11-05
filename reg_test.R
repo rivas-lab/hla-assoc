20212f1b14ef916145bf71178e17b555596a174c
Julia Eve Olivieri <jolivier@sh-ln02.stanford.edu>
2017-11-04 23:09:50 -0700

    added bma

--- reg_test.R
+++ reg_test.R
@@ -6,8 +6,11 @@ print("start")
 options(warn=1)
 
 test <- F
-run_rounded <- T
-run_dosage <- T
+run_rounded <- F
+run_dosage <- F
+rounded_add <- F
+rounded_factor <- F
+run_BMA <- T
 print_gene <- T
 t0 <- proc.time()
 #ukbb_files <- "$SCRATCH/ukbb_files/"
@@ -84,47 +87,75 @@ if (run_rounded) {
     #for (gene in colnames(dosage)) {
     #    print(length(unique(dosage[,gene])))
     #}
+    if (rounded_add) {
+#        for (gene in colnames(dosage[,1:30])) {
+        for (gene in colnames(dosage)) {
+            if (print_gene) {
+                print(gene)
+            }
+            #print(paste0("gcounts rows: ", nrow(covars[,"gcounts"])))
+            #print(paste0("dosage rows: ", nrow(as.numeric(dosage[,gene]))))
+        	covars["gcounts"] <- as.numeric(dosage[,gene])
+        
+        	fit <- summary(glm(status ~ as.numeric(gcounts) + age + sex + Array + PC1 + PC2 + PC3 + PC4,
+        		       family="binomial", data=covars))
+        	fit$deviance.resid <- NULL
+        	results.add[[gene]] <- fit
 
-#    for (gene in colnames(dosage[,1:30])) {
-    for (gene in colnames(dosage)) {
-        if (print_gene) {
-            print(gene)
         }
-        #print(paste0("gcounts rows: ", nrow(covars[,"gcounts"])))
-        #print(paste0("dosage rows: ", nrow(as.numeric(dosage[,gene]))))
-    	covars["gcounts"] <- as.numeric(dosage[,gene])
-    
-    	fit <- summary(glm(status ~ as.numeric(gcounts) + age + sex + Array + PC1 + PC2 + PC3 + PC4,
-    		       family="binomial", data=covars))
-    	fit$deviance.resid <- NULL
-    	results.add[[gene]] <- fit
+        saveRDS(results.add, paste0("reg_results/", outname, "_rounded_add.rds"))
 
     }
+    if (rounded_factor) {
+        # only include columns with more than 2 values (not including NA)
+        unique_counts <- apply(dosage, 2, function(x)length(unique(x[!is.na(x)])))
+        dosage <- dosage[,unique_counts > 2]
+        print(paste0("number excluded (rounded factor):",362 - ncol(dosage)))
 
-    # only include columns with more than 2 values (not including NA)
-    unique_counts <- apply(dosage, 2, function(x)length(unique(x[!is.na(x)])))
-    dosage <- dosage[,unique_counts > 2]
-    print(paste0("number excluded (rounded factor):",362 - ncol(dosage)))
-
-#    for (gene in colnames(dosage[,1:30])) {
-    for (gene in colnames(dosage)) {
-        if (print_gene) {
-            print(gene) 
+#        for (gene in colnames(dosage[,1:30])) {
+        for (gene in colnames(dosage)) {
+            if (print_gene) {
+                print(gene) 
+            }
+        	covars["gcounts"] <- as.factor(dosage[,gene])
+            n_levs <- nlevels(covars["gcounts"][which(!covars["gcounts"] == 0),])
+           # print(covars["gcounts"][which(!covars["gcounts"] == 0),])
+           # print(n_levs) 
+            if (n_levs > 1) {
+        	    fit <- summary(glm(status ~ as.factor(gcounts) + age + sex + Array + PC1 + PC2 + PC3 + PC4,
+        	    	       family="binomial", data=covars))
+        	    fit$deviance.resid <- NULL
+        	    results.gen[[gene]] <- fit
+            }
         }
-    	covars["gcounts"] <- as.factor(dosage[,gene])
-        n_levs <- nlevels(covars["gcounts"][which(!covars["gcounts"] == 0),])
-       # print(covars["gcounts"][which(!covars["gcounts"] == 0),])
-       # print(n_levs) 
-        if (n_levs > 1) {
-    	    fit <- summary(glm(status ~ as.factor(gcounts) + age + sex + Array + PC1 + PC2 + PC3 + PC4,
-    	    	       family="binomial", data=covars))
-    	    fit$deviance.resid <- NULL
-    	    results.gen[[gene]] <- fit
-        }
+        
+        saveRDS(results.gen, paste0("reg_results/", outname, "_rounded_gen.rds"))
     }
-    
-    saveRDS(results.gen, paste0("reg_results/", outname, "_rounded_gen.rds"))
-    saveRDS(results.add, paste0("reg_results/", outname, "_rounded_add.rds"))
 }
 
-print(proc.time() - t0)
+if (run_BMA) {
+    library("BMA")
+    #print(dosage)
+    print("starting BMA")
+    dosage = cbind(dosage, covars)
+#    dosage = subset(dosage, select = -c(status))
+    #dosage = subset(dosage, select = c(A_101, B_801, C_701)) 
+    dosage = dosage[,1:50]
+    #print(dosage)
+
+#    print(covars["status"])
+#    y <- as.vector(covars["status"])
+#    print(y)
+    glm.out.FF <- bic.glm(dosage, covars$status, strict = FALSE, OR = 30,
+        glm.family="binomial", factor.type=FALSE) 
+    summary(glm.out.FF)
+    print(glm.out.FF$probne0)
+    #png("bicglm.png", type="quartz")
+    imageplot.bma(glm.out.FF)
+    #dev.off()
+    write(ncol(dosage), file="timing.txt", append=TRUE)
+}
+
+#print(proc.time() - t0)
+write(proc.time() - t0, file="timing.txt", append=TRUE)
+#write(paste(ncol(dosage), proc.time, sep=","), file="timing.txt", append=TRUE)
